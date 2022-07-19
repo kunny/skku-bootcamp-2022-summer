38a516d6443e4407d2971c790a736e6f469c8984
Taeho Kim <jyte82@gmail.com>
2022-07-20 08:29:55 +0900

    Update formatting

--- sticky_notes/step4/lib/main.dart
+++ sticky_notes/step4/lib/main.dart
@@ -11,23 +11,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Sticky Notes',
-        theme: ThemeData(
-          primarySwatch: Colors.teal,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        initialRoute: NoteListPage.routeName,
-        routes: {
-          NoteListPage.routeName: (context) => const NoteListPage(),
-          NoteEditPage.routeName: (context) {
-            final args = ModalRoute.of(context)!.settings.arguments;
-            final index = args != null ? args as int : null;
-            return NoteEditPage(index);
-          },
-          NoteViewPage.routeName: (context) {
-            final index = ModalRoute.of(context)!.settings.arguments as int;
-            return NoteViewPage(index);
-          },
-        });
+      title: 'Sticky Notes',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: NoteListPage.routeName,
+      routes: {
+        NoteListPage.routeName: (context) => const NoteListPage(),
+        NoteEditPage.routeName: (context) {
+          final args = ModalRoute.of(context)!.settings.arguments;
+          final index = args != null ? args as int : null;
+          return NoteEditPage(index);
+        },
+        NoteViewPage.routeName: (context) {
+          final index = ModalRoute.of(context)!.settings.arguments as int;
+          return NoteViewPage(index);
+        },
+      },
+    );
   }
 }
--- sticky_notes/step5/lib/main.dart
+++ sticky_notes/step5/lib/main.dart
@@ -15,23 +15,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Sticky Notes',
-        theme: ThemeData(
-          primarySwatch: Colors.teal,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        initialRoute: NoteListPage.routeName,
-        routes: {
-          NoteListPage.routeName: (context) => const NoteListPage(),
-          NoteEditPage.routeName: (context) {
-            final args = ModalRoute.of(context)!.settings.arguments;
-            final id = args != null ? args as int : null;
-            return NoteEditPage(id);
-          },
-          NoteViewPage.routeName: (context) {
-            final id = ModalRoute.of(context)!.settings.arguments as int;
-            return NoteViewPage(id);
-          },
-        });
+      title: 'Sticky Notes',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: NoteListPage.routeName,
+      routes: {
+        NoteListPage.routeName: (context) => const NoteListPage(),
+        NoteEditPage.routeName: (context) {
+          final args = ModalRoute.of(context)!.settings.arguments;
+          final id = args != null ? args as int : null;
+          return NoteEditPage(id);
+        },
+        NoteViewPage.routeName: (context) {
+          final id = ModalRoute.of(context)!.settings.arguments as int;
+          return NoteViewPage(id);
+        },
+      },
+    );
   }
 }
